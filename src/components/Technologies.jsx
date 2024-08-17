5e04d0334b28d6e09577ef913ea50859b9590cf9
thisarakavishka <thisarakavishka0@gmail.com>
2024-08-17 17:02:00 +0530

    Add technologies section

---
+++ src/components/Technologies.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import { RiReactjsLine } from "react-icons/ri"
+
+const Technologies = () => {
+    return (
+        <div className='border-b border-neutral-800 pb-24'>
+            <h2 className='my-20 text-center text-4xl'>Technologies</h2>
+            <div className='flex flex-wrap items-center justify-center gap-4'>
+                <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+                    <RiReactjsLine className='text-7xl text-cyan-400' />
+                </div>
+                <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+                    <RiReactjsLine className='text-7xl text-cyan-400' />
+                </div>
+                <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+                    <RiReactjsLine className='text-7xl text-cyan-400' />
+                </div>
+                <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+                    <RiReactjsLine className='text-7xl text-cyan-400' />
+                </div>
+                <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+                    <RiReactjsLine className='text-7xl text-cyan-400' />
+                </div>
+                <div className='rounded-2xl border-4 border-neutral-800 p-4'>
+                    <RiReactjsLine className='text-7xl text-cyan-400' />
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Technologies
