138b356f4a074d3f8dddb54d23d03cac1249dd9d
thisarakavishka <thisarakavishka0@gmail.com>
2024-08-17 16:38:17 +0530

    add hero section for portfolio

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Navbar from './components/Navbar'
+import Hero from './components/Hero';
 
 const App = () => {
   return (
@@ -7,7 +8,8 @@ const App = () => {
       <div className='flixed top-0 -z-10 h-full w-full'></div>
       <div className="absolute top-0 z-[-2] h-screen w-screen bg-neutral-950 bg-[radial-gradient(ellipse_80%_80%_at_50%_-20%,rgba(120,119,198,0.3),rgba(255,255,255,0))]"></div>
       <div className='container mx-auto px-8'>
-      <Navbar />
+        <Navbar />
+        <Hero />
       </div>
     </div>
   );
