ccdf3aa756bcb27484de92aa36f6372c1da08a47
thisarakavishka <thisarakavishka0@gmail.com>
2024-08-17 17:22:49 +0530

    add experience section

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Navbar from './components/Navbar'
 import Hero from './components/Hero';
 import About from './components/About';
 import Technologies from './components/Technologies';
+import Experience from './components/Experience';
 
 const App = () => {
   return (
@@ -15,6 +16,7 @@ const App = () => {
         <Hero />
         <About />
         <Technologies />
+        <Experience />
       </div>
     </div>
   );
